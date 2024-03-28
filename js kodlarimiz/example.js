d01f8445a94df3846bf8cddb26637e4dcf3d9dbe
sinemablak <sinemablakk1@gmail.com>
2024-03-28 12:52:18 +0300

    Update example.js

--- js kodlarimiz/example.js
+++ js kodlarimiz/example.js
@@ -5,4 +5,7 @@ var sayi3 = 1; //güncel versiyonda artik kullanilmiyor
 console.log(sayi); //ekrana yazdirmak icin kullanilir.
 console.log(sayi2);
 
+//0-1 arasinda random sayi uretmek
+console.log(Math.random());
+
 //kosullu ifadeler if-else if-else
