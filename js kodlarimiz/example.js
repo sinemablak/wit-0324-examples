bc10fe9aa1d772025731c3b3ca592653c4bb0d07
sinemablak <sinemablakk1@gmail.com>
2024-03-28 12:49:05 +0300

    Update example.js

--- js kodlarimiz/example.js
+++ js kodlarimiz/example.js
@@ -3,3 +3,6 @@ let sayi = 5; //sonradan degisebilen variable
 const sayi2 = 3; //sonradan degisemeyen variable
 var sayi3 = 1; //güncel versiyonda artik kullanilmiyor
 console.log(sayi); //ekrana yazdirmak icin kullanilir.
+console.log(sayi2);
+
+//kosullu ifadeler if-else if-else
