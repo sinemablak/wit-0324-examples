03e9a9fdb03790b85af987745ceae14732bffdd0
sinemablak <sinemablakk1@gmail.com>
2024-03-28 12:40:48 +0300

    first commit

---
+++ js kodlarimiz/example.js
@@ -0,0 +1,5 @@
+//variable tanimlama
+let sayi = 5; //sonradan degisebilen variable
+const sayi2 = 3; //sonradan degisemeyen variable
+var sayi3 = 1; //güncel versiyonda artik kullanilmiyor
+console.log(sayi); //ekrana yazdirmak icin kullanilir.
